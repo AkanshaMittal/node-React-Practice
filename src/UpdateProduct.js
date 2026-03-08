46d0626aa9f319d094b70d1d21fa79b59dc27bf0
AkanshaMittal <akanshamittal210@gmail.com>
2026-03-08 20:00:13 +0530

    format

--- src/UpdateProduct.js
+++ src/UpdateProduct.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 const UpdateProduct = () => {
-  const [name,setName] = useState("");
-     const [price,setPrice] = useState("");
-      const [category,setCategory] = useState("");
-        const [company,setCompany] = useState("");
-        const [error,setError]     =  useState(false);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [company, setCompany] = useState("");
+  const [error, setError] = useState(false);
 
-         console.log({name,price,category,company})
+  console.log({ name, price, category, company })
   return (
     <div className="profile-container">
       <h1>Update Product</h1>
@@ -18,38 +18,38 @@ const UpdateProduct = () => {
         name="productname"
         placeholder="Enter Product Name"
 
-        onChange={(e)=>{setName(e.target.value)}}
+        onChange={(e) => { setName(e.target.value) }}
         value={name}
       />
-      
+
       <input
         className="inputBox"
         type="text"
         name="price"
         placeholder="Enter Product Price"
-        onChange={(e)=>{setPrice(e.target.value)}}
+        onChange={(e) => { setPrice(e.target.value) }}
         value={price}
       />
-      
+
       <input
         className="inputBox"
         type="text"
         name="category"
         placeholder="Enter Product Category"
-        onChange={(e)=>{setCategory(e.target.value)}}
+        onChange={(e) => { setCategory(e.target.value) }}
         value={category}
       />
-       
+
       <input
         className="inputBox"
         type="text"
         name="company"
         placeholder="Enter Product company"
-        onChange={(e)=>{setCompany(e.target.value)}}
+        onChange={(e) => { setCompany(e.target.value) }}
         value={company}
       />
-      
-      <button onClick={()=>{console.log("clicked....")}}className="submitBtn" type="submit">
+
+      <button onClick={() => { console.log("clicked....") }} className="submitBtn" type="submit">
         Update Product
       </button>
     </div>
